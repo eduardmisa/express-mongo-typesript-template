847b199b548b843d04642a537e4261b0379380d4
EduardMisa <eduardo@tirsolutions.com>
2020-05-05 14:10:39 +0800

    cleaned unused imports

--- src/datalayer/user.datalayer.ts
+++ src/datalayer/user.datalayer.ts
@@ -1,6 +1,5 @@
 import {ZDataLayer} from "./z.datalayer"
 import {User} from "../models/user.model"
-import {ObjectID} from "mongodb"
 
 export class UserDatalayer extends ZDataLayer<User> {
   constructor() {
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -9,35 +9,35 @@ router.get('/',
  async function(req: Request, res: Response, next: NextFunction) {
   let result = await new UserDatalayer().list()
   if (result) res.send(result)
-  res.status(400).send("Server error")
+  else res.status(400).send("Server error")
 })
 
 router.get('/:slug',
  async function(req: Request, res: Response, next: NextFunction) {
   let result = await new UserDatalayer().get(new ObjectID(req.params.slug))
   if (result) res.send(result)
-  res.status(400).send("Server error")
+  else res.status(400).send("Server error")
 })
 
 router.post('/',
  async function(req: Request, res: Response, next: NextFunction) {
   let result = await new UserDatalayer().add(req.body as User)
   if (result) res.send(result)
-  res.status(400).send("Server error")
+  else res.status(400).send("Server error")
 })
 
 router.put('/:slug',
  async function(req: Request, res: Response, next: NextFunction) {
   let result = await new UserDatalayer().edit(new ObjectID(req.params.slug), req.body as User)
   if (result) res.send(result)
-  res.status(400).send("Server error")
+  else res.status(400).send("Server error")
 })
 
 router.delete('/:slug',
  async function(req: Request, res: Response, next: NextFunction) {
   let result = await new UserDatalayer().delete(new ObjectID(req.params.slug))
   if (result) res.send(result)
-  res.status(400).send("Server error")
+  else res.status(400).send("Server error")
 })
 
 module.exports = router;
